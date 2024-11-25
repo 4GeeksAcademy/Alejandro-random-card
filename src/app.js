28be251896e54466838153de655622481ee7423e
Alejandro Quiles García <alexqg1208@gmail.com>
2024-11-25 19:02:04 +0000

    final commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = ["♦", "♥", "♠", "♣"];
+  const numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+
+  const paloSeleccionado = palos[Math.floor(Math.random() * palos.length)];
+  const numeroSeleccionado =
+    numeros[Math.floor(Math.random() * numeros.length)];
+
+  const paloArriba = document.querySelector(".palo-arriba");
+  const numeroElemento = document.querySelector(".numero");
+  const paloAbajo = document.querySelector(".palo-abajo");
+
+  paloArriba.textContent = paloSeleccionado;
+  numeroElemento.textContent = numeroSeleccionado;
+  paloAbajo.textContent = paloSeleccionado;
+
+  if (paloSeleccionado === "♥" || paloSeleccionado === "♦") {
+    paloArriba.style.color = "red";
+    paloAbajo.style.color = "red";
+  } else {
+    paloArriba.style.color = "black";
+    paloAbajo.style.color = "black";
+  }
 };
